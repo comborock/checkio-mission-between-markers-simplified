5a28c54ba6e1d80b5be30a874dbcc446364f5cd3
Alexander Lyabah <a.lyabah@checkio.org>
2019-01-29 01:13:13 +0200

    simplified

--- verification/tests.py
+++ verification/tests.py
@@ -15,41 +15,22 @@ TESTS = {
             "answer": 'apple'
         },
         {
-            "input": ["<head><title>My new site</title></head>",
-                      "<title>", "</title>"],
-            "answer": "My new site",
-            "explanation": "markers can be longer than one symbol"
+            "input": ['What is [apple]', '[', ']'],
+            "answer": 'apple'
         },
         {
-            "input": ['No[/b] hi', '[b]', '[/b]'],
-            "answer": 'No',
-            "explanation": "No beginning marker"
+            "input": ['What is ><', '>', '<'],
+            "answer": ''
         },
         {
-            "input": ['No [b]hi', '[b]', '[/b]'],
-            "answer": 'hi',
-            "explanation": "No ending marker"
-        },
-        {
-            "input": ['No hi', '[b]', '[/b]'],
-            "answer": 'No hi',
-            "explanation": "No markers at all"
-        },
-        {
-            "input": ['No <hi>', '>', '<'],
-            "answer": '',
-            "explanation": "Wrong direction"
+            "input": ['[an apple]', '[', ']'],
+            "answer": 'an apple'
         }
     ],
     "Extra": [
         {
-            "input": ["Never send a human to do a machine's job.", "Never", "do"],
-            "answer": " send a human to "
-        },
-        {
-            "input": ['No <hi> one', '>', '<'],
-            "answer": '',
-            "explanation": "Wrong direction2"
+            "input": ['>Apple< is simple', '>', '<'],
+            "answer": 'apple'
         }
     ]
 }
